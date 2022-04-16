063cf8f4bb6be7bd46f253d78dd9f78ed21368b9
atharv <atharv.tiwari@outlook.com>
2022-04-16 22:07:06 +0530

    Revert "vscode stuff"
    This reverts commit 819c3b0c9c11c8f90813c481d1ba632e28f09aba.

--- tag_bag/tag_bag.ino
+++ tag_bag/tag_bag.ino
@@ -1,40 +1,38 @@
 int forcePin = D2;
 int forceReading;
 
-void setup()
-{
-	// put your setup code here, to run once:
-	Serial.begin(9600);	 // Serial Monitor for FSR readings
-	pinMode(5, OUTPUT);	 // LED
-	pinMode(14, OUTPUT); // Fan
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // Serial Monitor for FSR readings
+  pinMode(5, OUTPUT); // LED
+  pinMode(14, OUTPUT); // Fan
 }
 
-void loop()
-{
-	// put your main code here, to run repeatedly:
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // Start with LED and fan turned off
+  digitalWrite(5, LOW);
+  digitalWrite(14, LOW);
 
-	// Start with LED and fan turned off
-	digitalWrite(5, LOW);
-	digitalWrite(14, LOW);
+  // Reading value from FSR
+  forceReading = analogRead(forcePin);
+  
+  // Logging value from FSR in Serial Monitor
+  Serial.print("Analog reading = ");
+  Serial.println(forceReading);
 
-	// Reading value from FSR
-	forceReading = analogRead(forcePin);
+  if (forceReading)
+  {
+    digitalWrite(5, HIGH); // Turn LED on
+    digitalWrite(14, HIGH); // Turn Fan on
+    delay(100);
+  }
+  else
+  {
+    digitalWrite(5, LOW); // Turn LED off
+    digitalWrite(14, LOW); // Turn Fan off
+  }
 
-	// Logging value from FSR in Serial Monitor
-	Serial.print("Analog reading = ");
-	Serial.println(forceReading);
-
-	if (forceReading)
-	{
-		digitalWrite(5, HIGH);	// Turn LED on
-		digitalWrite(14, HIGH); // Turn Fan on
-		delay(100);
-	}
-	else
-	{
-		digitalWrite(5, LOW);  // Turn LED off
-		digitalWrite(14, LOW); // Turn Fan off
-	}
-
-	delay(100);
+  delay(100);
 }
