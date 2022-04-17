2776ed20fe5c23b662bdcc53eab27891a16adea0
atharv <atharv.tiwari@outlook.com>
2022-04-17 15:52:47 +0530

    Added RFID functionality

--- tag_bag/tag_bag.ino
+++ tag_bag/tag_bag.ino
@@ -1,40 +1,142 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN D8
+#define RST_PIN D0
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID
+byte nuidPICC[4];
+
 int forcePin = D2;
 int forceReading;
 
 void setup()
 {
-	// put your setup code here, to run once:
-	Serial.begin(9600);	 // Serial Monitor for FSR readings
-	pinMode(5, OUTPUT);	 // LED
-	pinMode(14, OUTPUT); // Fan
+  // put your setup code here, to run once:
+
+  Serial.begin(115200); // Serial Monitor for FSR readings
+  SPI.begin();          // Init SPI bus
+  rfid.PCD_Init();      // Init MFRC522
+  Serial.println();
+  Serial.print(F("Reader :"));
+  rfid.PCD_DumpVersionToSerial();
+
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println();
+  Serial.println(F("This code scan the MIFARE Classic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+  
+  pinMode(2, OUTPUT); // LED
+  pinMode(5, OUTPUT); // Fan
 }
 
 void loop()
 {
-	// put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
 
-	// Start with LED and fan turned off
-	digitalWrite(5, LOW);
-	digitalWrite(14, LOW);
+  digitalWrite(2, LOW); // Turn LED off
+  digitalWrite(5, LOW); // Turn Fan off
 
-	// Reading value from FSR
-	forceReading = analogRead(forcePin);
+  // Reading value from FSR
+  forceReading = analogRead(forcePin);
 
-	// Logging value from FSR in Serial Monitor
-	Serial.print("Analog reading = ");
-	Serial.println(forceReading);
+  if (forceReading)
+  {
+    // Logging value from FSR in Serial Monitor
+    Serial.print("Analog reading = ");
+    Serial.println(forceReading);
+    digitalWrite(2, HIGH); // Turn LED on
+    digitalWrite(5, HIGH); // Turn Fan on
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(2, LOW); // Turn LED off
+    digitalWrite(5, LOW); // Turn Fan off
+  }
 
-	if (forceReading)
-	{
-		digitalWrite(5, HIGH);	// Turn LED on
-		digitalWrite(14, HIGH); // Turn Fan on
-		delay(100);
-	}
-	else
-	{
-		digitalWrite(5, LOW);  // Turn LED off
-		digitalWrite(14, LOW); // Turn Fan off
-	}
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
 
-	delay(100);
+  // Verify if the NUID has been readed
+  if (!rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+  {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+      rfid.uid.uidByte[1] != nuidPICC[1] ||
+      rfid.uid.uidByte[2] != nuidPICC[2] ||
+      rfid.uid.uidByte[3] != nuidPICC[3])
+  {
+    Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++)
+    {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+
+    Serial.println(F("The NUID tag is:"));
+    Serial.print(F("In hex: "));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+
+  else
+    Serial.println(F("Card read previously."));
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+
+  delay(1000);
+}
+
+/**
+   Helper routine to dump a byte array as hex values to Serial.
+*/
+void printHex(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+   Helper routine to dump a byte array as dec values to Serial.
+*/
+void printDec(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
 }
